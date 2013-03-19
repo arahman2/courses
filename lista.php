33a18e66ade9afc4f1e349caf318f28c96ecb6f7
arahman2 <abdullah.rahmanyar@gmail.com>
2013-03-19 00:23:19 -0400

    adding new files for new table trinkle ratings

---
+++ lista.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Trinkle Hall Ratings</title>
+<link rel="stylesheet" href="styles.css" type="text/css" />
+
+
+</head>
+<body>
+<?php
+   include('header.html');
+?>
+
+
+<div id="wrap">
+	<div class="pagewrapper">
+		<div class="innerpagewrapper">
+			<div class="page">
+				<div class="content">
+				
+					<!-- CONTENT -->
+					<h3>Current Trinkle Hall Ratings</h3>
+					
+<table>		
+<tr><th>Name</th><th>Comments</th><th>Rating</th></tr>
+<?php
+	include('dbconnect.php');
+	$query = "SELECT name, comments FROM trinkle ORDER BY name";
+    $result = mysqli_query($db, $query)
+                         or die("Error Querying Database");
+    while($row = mysqli_fetch_array($result)) {
+  		$name = $row['name'];
+  		$comments = $row['comments'];
+  	echo "<tr><td>$name</td><td>$comments</td></tr>\n";
+  }                 
+                         
+                         
+                         
+    mysqli_close($db);
+
+?>
+</table>
+					<!-- END CONTENT -->
+					
+				</div>
+				
+				<?php
+				    include('SIDEnFOOTER.html');
+				?>
+				   
+
+
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
---
+++ reporta.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>UMW Course Ratings</title>
+<link rel="stylesheet" href="styles.css" type="text/css" />
+
+
+</head>
+<body>
+<?php
+   include('header.html');
+?>
+
+
+<div id="wrap">
+	<div class="pagewrapper">
+		<div class="innerpagewrapper">
+			<div class="page">
+				<div class="content">
+				
+					<!-- CONTENT -->
+					<h3>Rate Trinkle Hall</h3>
+					<p>Welcome to the Trinkle Hall ratings Website for UMW</p>
+					<form method = "post" action = "report2a.php">
+					<table>
+					<tr><td>Name</td><td><input type="text" id="name" name="name" /></td></tr>
+										
+					<tr><td>Rate Trinkle Hall</td><td>Life Threatening <input type="range" name="rating" min="1" max="10" step="1" value="5"/>Unicorns and Rainbows</td></tr>
+					<tr><td>Comments: </td><td><textarea rows="5" cols="25" name="comments" id="comments"></textarea></td></tr>
+					<tr><td>&nbsp;</td><td><input type="submit" value="Post Rating" /></td></tr>
+					</table>
+					
+					</form>
+					<!-- END CONTENT -->
+					
+				</div>
+				
+				<?php
+				    include('SIDEnFOOTER.html');
+				?>
+				   
+
+
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>

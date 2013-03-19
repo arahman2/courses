69a1d24ca67c651b995151be2a05c7f6d621c50b
arahman2 <abdullah.rahmanyar@gmail.com>
2013-03-18 23:14:06 -0400

    changed header and added contact.php

---
+++ contact.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>UMW Course Ratings</title>
+<link rel="stylesheet" href="styles.css" type="text/css" />
+</head>
+<body>
+
+<?php
+  include("header.html");
+  
+  ?>
+<div id="wrap">
+	<div class="pagewrapper">
+		<div class="innerpagewrapper">
+			<div class="page">
+				<div class="content">
+				
+					<!-- CONTENT -->
+					<h3>Welcome!</h3>
+					<p>Welcome to the to UMW course ratings website. </p>
+					
+					<p>Don't you just hate it when you take a course and 
+					expect something yet receive a completely different 
+					experience. Well we have designed this website for 
+					students just like you. This website allows current 
+					UMW students and alumni to rate the courses that they
+					have taken or are currently enrolled in. We strive to
+					provide our users with the most realistic expectations
+					of courses offered here at UMW. You can also post your 
+					own comments and feedback about courses and professors.</p>
+					<img src="images/ufo_003.jpg" />
+		
+                     <p>&nbsp;</p>
+					<h3>About Us</h3>
+					<p><b>We are all current CPSC students at UMW...</b></p>
+					<p>Abdullah Rahmanyar - is the brains of the operation</p>
+					<p>Oceana Peemoeller - is the one who is always in dire need of coffee</p>
+					<p>Norah Almahdali - keeps everything organized and in order</p>
+					<!-- END CONTENT -->
+					
+				</div>
+<?php
+  include("SIDEnFOOTER.html");
+  
+  ?>
+
+
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
